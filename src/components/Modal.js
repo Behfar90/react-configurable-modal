d0710cd1736982d919c885114262cb7a91b577e5
BeHFaR <behfar.behzad@webstep.no>
2022-08-01 11:32:52 +0200

    Took out the children + Added width/height as props

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,36 +3,32 @@ import PropTypes from "prop-types";
 import useOutsideCloser from "../hooks/useOutsideCloser";
 import "../styles.css";
 
-const Modal = ({ show, setShow }) => {
+const Modal = ({ show, setShow, children, width, height }) => {
   const modalRef = useRef();
   const modalCloser = () => setShow(false);
 
   useOutsideCloser(modalRef, modalCloser);
 
+  const passedStyles = {
+    width: width || null,
+    height: height || null,
+  };
+
   return (
     <div ref={modalRef} className={"modal" + (show ? " modal__show" : "")}>
-      <div className="modal__content">
-        <div className="modal__header">
-          <span onClick={modalCloser} className="modal__close">
-            &times;
-          </span>
-          <h2>Modal Header</h2>
-        </div>
-        <div className="modal__body">
-          <p>Some text in the Modal Body</p>
-          <p>Some other text...</p>
-        </div>
-        <div className="modal__footer">
-          <h3>Modal Footer</h3>
-        </div>
+      <div className="modal__content" style={passedStyles}>
+        {children}
       </div>
     </div>
   );
 };
 
 Modal.propTypes = {
-  show: PropTypes.bool,
-  setShow: PropTypes.func,
+  show: PropTypes.bool.isRequired,
+  setShow: PropTypes.func.isRequired,
+  children: PropTypes.node,
+  width: PropTypes.string,
+  height: PropTypes.string,
 };
 
 export default Modal;
--- src/components/Test.js
+++ src/components/Test.js
@@ -6,7 +6,29 @@ const Test = () => {
 
   return (
     <div>
-      <Modal show={modalShow} setShow={modalSetShow} />
+      <Modal
+        show={modalShow}
+        setShow={modalSetShow}
+        width={"50%"}
+        height={"550px"}
+      >
+        <div className="modal__header">
+          <span
+            onClick={() => modalSetShow(!modalShow)}
+            className="modal__close"
+          >
+            &times;
+          </span>
+          <h2>Modal Header</h2>
+        </div>
+        <div className="modal__body">
+          <p>Some text in the Modal Body</p>
+          <p>Some other text...</p>
+        </div>
+        <div className="modal__footer">
+          <h3>Modal Footer</h3>
+        </div>
+      </Modal>
       <button onClick={() => modalSetShow(!modalShow)}>Click</button>
     </div>
   );
