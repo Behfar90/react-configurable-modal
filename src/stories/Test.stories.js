fddbbc5c7492b2e0b42721fc81770699a9257589
BeHFaR <behfar.behzad@webstep.no>
2022-08-22 14:19:03 +0200

    Removed the Test stories page

--- src/stories/Test.stories.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Test from "../components/Test";
-
-const stories = storiesOf("Modal test", module);
-stories.add("Test", () => {
-  return <Test />;
-});
