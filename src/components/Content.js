e2e76bb7cd8e745498bc720ac4067b92026e48aa
BeHFaR <behfar.behzad@webstep.no>
2022-08-23 12:53:26 +0200

    Fixed the animate 'zoom-in' and 'fade-in' class names

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,16 +5,22 @@ const Content = ({ animation, position, passedStyles, children }, ref) => {
   var newPosition = useViewportKeeper(ref);
   const newStyles = { ...passedStyles, ...newPosition };
 
+  const animationClass = ["top", "bottom"].includes(animation)
+    ? " animate__vertical"
+    : ["left", "right"].includes(animation)
+    ? " animate__horizontal"
+    : ["fade-in", "zoom-in"].includes(animation)
+    ? ` animate__${animation}`
+    : "";
+
+  const positionClass =
+    position && position !== "top" ? ` position__${position}` : "";
+
   return (
     <div
       ref={ref}
       id="modalContent"
-      className={
-        "modal__content" +
-        (["top", "bottom"].includes(animation) ? " animate__vertical" : "") +
-        (["left", "right"].includes(animation) ? " animate__horizontal" : "") +
-        (position && position !== "top" ? ` position__${position}` : "")
-      }
+      className={"modal__content" + animationClass + positionClass}
       style={passedStyles} // passedStyles for now until a new solution for the viewportKeeper is found
     >
       {children}
